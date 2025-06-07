1f436bf0399625d4220403357a3d785851ef9b3b
Chinta Karina K <chinta.karina.tif24@polban.ac.id>
2025-06-07 22:45:06 +0700

    merapihkan pelanggan.h

--- pelanggan.h
+++ pelanggan.h
@@ -1,21 +1,16 @@
+#ifndef PELANGGAN_H
+#define PELANGGAN_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "Keranjang.h"
-#define MAX_ID_PELANGGAN 40
-#ifndef PELANGGAN_H
-#define PELANGGAN_H
 
 typedef struct Pelanggan {
     char nama[50];
     Keranjang* KPelanggan;
 } Pelanggan;
 
-Pelanggan* CreatePelanggan (const char* id);
-void FreePelanggan (Pelanggan* pelanggan);
-
-
-
-
-
+Pelanggan* CreatePelanggan(const char* nama);
+void FreePelanggan(Pelanggan* p);
 
 #endif
