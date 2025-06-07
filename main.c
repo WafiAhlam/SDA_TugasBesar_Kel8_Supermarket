1f37578bc14516f07cc3fe459f35df09a0989c4b
marrelykasih <marrely.salsa.tif24@polban.ac.id>
2025-06-07 13:35:13 +0700

    main bagian keranjang

--- main.c
+++ main.c
@@ -10,5 +10,41 @@
 //Queue antrianPelanggan;       // pelanggan yang lagi ngantri
 //Queue pelangganSelesai;       // pelanggan yang udah selesai belanja
 
+//keranjang kasih 
+ Keranjang* keranjang = createKeranjang();
+
+  
+    char inputID[MAX_ID_BARANG];
+    printf("Masukkan ID barang yang ingin dibeli: ");
+    scanf("%s", inputID);
+
+    AddressItemNode found = SearchItem(listMakanan, inputID);
+    if (found != NULL) {
+        pushKeranjang(keranjang, &found->dataBarang);
+        printf("Barang berhasil dimasukkan ke keranjang.\n");
+    } else {
+        printf("Barang tidak ditemukan.\n");
+    }
+
+
+    displayKeranjang(keranjang);
+
+
+    printf("\nMengeluarkan barang terakhir dari keranjang...\n");
+    item* dikeluarkan = popKeranjang(keranjang);
+    if (dikeluarkan != NULL) {
+        printf("Barang '%s' dikeluarkan dari keranjang.\n", dikeluarkan->namabarang);
+    }
+
+    displayKeranjang(keranjang);
+
+
+    double total = getTotalHargaKeranjang(keranjang);
+    printf("Total belanja sekarang: %.2f\n", total);
+
+    clearKeranjang(keranjang);
+    free(keranjang);
+
+    return 0;
 
 
